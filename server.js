8f1d48753a7a394b5786d2b38c16b5cd52a7cdd1
Seungjun <seungjun.kim0906@gmail.com>
2021-12-13 02:20:29 -0500

    second commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const { sequelize } = require("./models/models");
 const http = require("http");
 const app = require("./app");
 
+const PORT = process.env.PORT || 5000;
+
 const server = http.createServer(app);
 
 async function startServer() {
@@ -12,3 +14,5 @@ async function startServer() {
     console.log(`Server running on port ${PORT}`);
   });
 }
+
+startServer();
